36ceb483135d9d04dea64c2fae13716505185f47
Tran Duy <tranduy10a@gmail.com>
2025-03-03 07:23:37 +0700

    first commit

---
+++ src/ProjectCrypto/Project/Main.java
@@ -0,0 +1,27 @@
+package ProjectCrypto.Project;
+
+public class Main {
+    public static void main(String[] args) {
+        String plainText = "HOW MUCH WOOD CAN A WOODCHUCK CHUCK IF A WOOD CHUCK COULD CHUCK";
+        String bellasoStr = "HELLO";
+        int caesarKey = 15;
+
+        // Kiểm tra mã hóa và giải mã Caesar Cipher
+        System.out.println("Caesar Cipher Test:");
+        String encryptedCaesar = CryptoManager.caesarEncryption(plainText, caesarKey);
+        System.out.println("Encrypted Caesar: " + encryptedCaesar);
+
+        String decryptedCaesar = CryptoManager.caesarDecryption(encryptedCaesar, caesarKey);
+        System.out.println("Decrypted Caesar: " + decryptedCaesar);
+
+        // Kiểm tra mã hóa và giải mã Bellaso Cipher
+        System.out.println("\nBellaso Cipher Test:");
+        String encryptedBellaso = CryptoManager.bellasoEncryption(plainText, bellasoStr);
+        System.out.println("Encrypted Bellaso: " + encryptedBellaso);
+
+        String decryptedBellaso = CryptoManager.bellasoDecryption(encryptedBellaso, bellasoStr);
+        System.out.println("Decrypted Bellaso: " + decryptedBellaso);
+
+    }
+}
+
---
+++ src/ProjectCrypto/String/Main.java
@@ -0,0 +1,4 @@
+package ProjectCrypto.String;
+
+public class Main {
+}
